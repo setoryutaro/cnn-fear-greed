70a01461256f4512562b4b1a412222d69c0e1239
setoryutaro <141482002+setoryutaro@users.noreply.github.com>
2026-07-11 19:32:38 +0900

    Enhance fetch.js to wait for JSON response
    Wait for a specific JSON response before navigating to the new URL and added logging for response status and URL.

--- fetch.js
+++ fetch.js
@@ -1,15 +1,31 @@
 const { chromium } = require("playwright");
 
 (async () => {
+
   const browser = await chromium.launch({
     headless: true
   });
 
   const page = await browser.newPage();
 
-  await page.goto("https://example.com");
+  // JSONレスポンスを待つ
+  const responsePromise = page.waitForResponse(r =>
+    r.url().includes("/charts/data/50108")
+  );
 
-  console.log(await page.title());
+  await page.goto(
+    "https://en.macromicro.me/charts/50108/cnn-fear-and-greed",
+    {
+      waitUntil: "networkidle",
+      timeout: 120000
+    }
+  );
+
+  const response = await responsePromise;
+
+  console.log("STATUS =", response.status());
+  console.log("URL =", response.url());
 
   await browser.close();
+
 })();
