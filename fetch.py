44bb330425228f0084a9c09be028b4f591d92d8c
setoryutaro <141482002+setoryutaro@users.noreply.github.com>
2026-07-12 18:11:33 +0900

    Integrate Google Sheets to update FGI score
    Added Google Sheets integration to update the Fear & Greed Index score.

--- fetch.py
+++ fetch.py
@@ -1,5 +1,49 @@
+import json
+import os
+
 import fear_greed
+import gspread
+from google.oauth2.service_account import Credentials
 
+# -------------------------
+# Fear & Greed Index取得
+# -------------------------
 data = fear_greed.get()
+score = data["score"]
 
-print(data)
+print("FGI:", score)
+
+# -------------------------
+# Google認証
+# -------------------------
+service_account = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT"])
+
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+
+credentials = Credentials.from_service_account_info(
+    service_account,
+    scopes=scopes
+)
+
+gc = gspread.authorize(credentials)
+
+# -------------------------
+# スプレッドシートを開く
+# -------------------------
+SPREADSHEET_ID = "1iJNyzVfgRP8E_Y0qAF7NSMkwJmnRbgPRRduYi6vjyYE"
+
+worksheet = gc.open_by_key(
+    SPREADSHEET_ID
+).worksheet("FGI")
+
+# -------------------------
+# F2へ書き込み
+# -------------------------
+worksheet.update(
+    "F2",
+    [[score]]
+)
+
+print("Google Sheets Updated!")
